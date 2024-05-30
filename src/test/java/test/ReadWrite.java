1c6416e53f73436b3054955b607a212bd83f6467
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2024-05-30 12:13:22 +0100

    how to read and write properties

---
+++ src/test/java/test/ReadWrite.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadWrite {
+	public static void main(String[] args) throws IOException {
+
+		Properties properties = new Properties();
+		FileInputStream inputStream = new FileInputStream(
+				"C:\\Users\\Engr. Abbey\\eclipse-workspace\\ReadWriteFile\\src\\test\\resources\\properties\\testdata.properties");
+		properties.load(inputStream);
+		System.out.println(properties.getProperty("browser"));
+		System.out.println(properties.getProperty("url"));
+
+		FileOutputStream outputStream = new FileOutputStream(
+				"C:\\Users\\Engr. Abbey\\eclipse-workspace\\ReadWriteFile\\src\\test\\resources\\properties\\testdata.properties");
+		properties.setProperty("testdata", "1234567");
+		properties.store(outputStream, "This is the customer data");
+	}
+
+}
